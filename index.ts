98b39d16497cb5e9787f331d6bb400ba3399380b
Docenko Pasha <pashadocenko@gmail.com>
2023-10-24 22:16:32 +0300

    index.ts was initialized

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import express, {Express} from "express";
 import {config} from "dotenv";
+import cors from 'cors';
 
 config();
 
 const app: Express = express();
 const port: number = parseInt(process.env.PORT!, 10) || 5000;
+
+
+app.use(cors());
+app.use(express.json());
 
 const start = async () => {
     try {
