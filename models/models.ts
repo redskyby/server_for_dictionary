f55512262b9c356689609196e3f0a91c5a2fbf0d
Docenko Pasha <pashadocenko@gmail.com>
2023-11-22 22:08:33 +0300

    models.ts was done. Eslint was added.

--- models/models.ts
+++ models/models.ts
@@ -15,6 +15,76 @@ class Words extends Model{
 
 class Translations extends Model{
     public translationId!:number;
-    public translation! : string;
+    public translation1! : string;
+    public translation2! : string;
 }
 
+User.init(
+    {
+        userId : {
+            type : DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        email : {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        password : {
+            type : DataTypes.STRING
+        },
+        role : {
+            type : DataTypes.STRING,
+            defaultValue : "USER"
+        }
+    },
+    {
+        sequelize,
+        modelName : "user"
+    }
+);
+
+Words.init({
+        wordId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        word: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        }
+    },
+    {
+        sequelize,
+        modelName: "words"
+    }
+);
+
+Translations.init({
+    translationId : {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    translation1 :{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    translation2 :{
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+} , {
+    sequelize,
+    modelName: "translations"
+})
+
+User.belongsToMany(Words, { through: "UserWords" });
+Words.belongsToMany(User, { through: "UserWords" });
+
+Words.hasOne(Translations, { foreignKey: "wordId", onDelete: "CASCADE" });
+Translations.belongsTo(Words, { foreignKey: "wordId" });
+
+export { User, Words, Translations };
