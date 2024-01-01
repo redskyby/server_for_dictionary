0ea2152349a7f2d02538de68641e1fa6e28ce366
Docenko Pasha <pashadocenko@gmail.com>
2024-01-01 22:20:32 +0300

    there are two options in host. (db_sequelize.ts)

--- db_sequelize.ts
+++ db_sequelize.ts
@@ -5,7 +5,7 @@ config();
 
 const sequelize: Sequelize = new Sequelize(process.env.DATABASE!, process.env.USER!, process.env.PASSWORD!, {
     dialect: "mysql",
-    host: process.env.HOST!,
+    host: process.env.HOST || "192.168.56.10",
     port: parseInt(process.env.PORT_DB!, 10),
 });
 
