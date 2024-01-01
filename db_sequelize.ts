9d5d732525ccd581b259bfbaa9bd3c15b4149f59
Docenko Pasha <pashadocenko@gmail.com>
2024-01-01 20:54:08 +0300

    test connection to the database

--- db_sequelize.ts
+++ db_sequelize.ts
@@ -3,10 +3,17 @@ import { config } from "dotenv";
 
 config();
 
-const sequelize: Sequelize = new Sequelize(process.env.DATABASE!, process.env.USER!, process.env.PASSWORD!, {
+// const sequelize: Sequelize = new Sequelize(process.env.DATABASE!, process.env.USER!, process.env.PASSWORD!, {
+//     dialect: "mysql",
+//     host: process.env.HOST!,
+//     port: parseInt(process.env.PORT_DB!, 10),
+// });
+
+const sequelize: Sequelize = new Sequelize(process.env.DATABASE!, "root", "Docenko4493!", {
     dialect: "mysql",
     host: process.env.HOST!,
     port: parseInt(process.env.PORT_DB!, 10),
 });
+
 
 export default sequelize;
